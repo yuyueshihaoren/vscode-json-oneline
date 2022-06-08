a53fe12ee82e1d0ea404cef29f867ac711fe0c47
Yue Yu <yuyueshihaoren@gmail.com>
2022-06-08 02:18:39 -0700

    typo, format

--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,40 @@
-'use strict';
-import * as vscode from 'vscode';
+"use strict";
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('extension.convertToOneLine', function () {
-		// Get the active text editor
-		const editor = vscode.window.activeTextEditor;
+  const disposable = vscode.commands.registerCommand(
+    "extension.convertToOneLine",
+    function () {
+      // Get the active text editor
+      const editor = vscode.window.activeTextEditor;
 
-		if (editor) {
-			const document = editor.document;
-			if (document.languageId != 'json') {
-				vscode.window.showWarningMessage("Not a JSON file");
-				return;
-			}
-			const selection = editor.selection;
+      if (editor) {
+        const document = editor.document;
+        if (document.languageId != "json") {
+          vscode.window.showWarningMessage("Not a JSON file");
+          return;
+        }
+        const selection = editor.selection;
 
-			const selectedText = document.getText(selection);
-			try {
-				const textJson = JSON.parse(selectedText);
-				const textJsonOneline = JSON.stringify(textJson);
-				editor.edit(editBuilder => {
-					editBuilder.replace(selection, textJsonOneline);
-				});
-			} catch (e: unknown) {
-				if (typeof e === "string") {
-					vscode.window.showWarningMessage(e);
-				} else if (e instanceof Error) {
-					vscode.window.showWarningMessage(e.message);
-				}
-				
-				return;
-			}
-		}
-	});
+        const selectedText = document.getText(selection);
+        try {
+          const textJson = JSON.parse(selectedText);
+          const textJsonOneline = JSON.stringify(textJson);
+          editor.edit((editBuilder) => {
+            editBuilder.replace(selection, textJsonOneline);
+          });
+        } catch (e: unknown) {
+          if (typeof e === "string") {
+            vscode.window.showWarningMessage(e);
+          } else if (e instanceof Error) {
+            vscode.window.showWarningMessage(e.message);
+          }
 
-	context.subscriptions.push(disposable);
+          return;
+        }
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
